35a0fe760c8cf0692ae6a7732196e13a3377e9a8
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2019-04-26 05:32:39 -0300

    more generic component

--- app/components/EthAddress.jsx
+++ app/components/EthAddress.jsx
@@ -6,8 +6,8 @@ import './EthAddress.css';
 class EthAddress extends React.Component {
 	static propTypes = {
 		className: PropTypes.string,
-		defaultValue: PropTypes.string,
 		value: PropTypes.string,
+		defaultValue: PropTypes.string,
 		colors: PropTypes.bool,
 		blocky: PropTypes.bool,
 		blockySize: PropTypes.number,
@@ -18,7 +18,7 @@ class EthAddress extends React.Component {
 	};
 
 	static defaultProps = {
-		className: 'text-monospace',
+		className: 'text-monospace eth-address',
 		defaultValue: "0x0000000000000000000000000000000000000000",
 		colors: true,
 		control: false,
@@ -34,19 +34,18 @@ class EthAddress extends React.Component {
 		this.controlRef = React.createRef();
 		this.state = {
 			value: props.value != undefined ? props.value : props.defaultValue,
-
 		};
 	}
 
 
 	componentDidMount() {
 		if(this.props.control)
-        	this.controlRef.current.textContent = this.props.value;
+        	this.controlRef.current.textContent = this.state.value;
     }
 
 	componentDidUpdate(prevProps, prevState) {
-        if (prevProps.value != this.props.value && this.props.value != this.state.address) {
-            this.setState({address: this.props.value});
+        if (prevProps.value != this.props.value && this.props.value != this.state.value) {
+            this.setState({value: this.props.value});
         }
     }
 
@@ -61,7 +60,7 @@ class EthAddress extends React.Component {
 		if(text.length == 0){
 			text = "0x0000000000000000000000000000000000000000";
 		}
-		this.setState({ address: text });
+		this.setState({ value: text });
 		this.notifyListeners(text);
 	}
 	
@@ -81,9 +80,9 @@ class EthAddress extends React.Component {
             event.stopPropagation();
             event.preventDefault();
             this.controlRef.current.textContent = pastedData;
-            this.setState({ address: pastedData });
+            this.setState({ value: pastedData });
 		} else {
-			this.setState({ address: this.controlRef.current.textContent });
+			this.setState({ value: this.controlRef.current.textContent });
 		}
 		this.notifyListeners(this.controlRef.current.textContent);
     }
@@ -102,24 +101,24 @@ class EthAddress extends React.Component {
 			blockyScale,
 			control
 		} = this.props;
-		const { address } = this.state;
-		let valid = /^(0x)?[0-9a-f]{40}$/i.test(address);
+		const { value } = this.state;
+		let valid = /^(0x)?[0-9a-f]{40}$/i.test(value);
 		const colorStyle = colors && valid ? {
-			backgroundImage: `linear-gradient(90deg, #${address.substr(6, 6)} 0% 15%, #${address.substr(12, 6)} 17% 32%, #${address.substr(18, 6)} 34% 49%, #${address.substr(24, 6)} 51% 66%, #${address.substr(30, 6)} 68% 83%, #${address.substr(36, 6)} 85% 100%)`
+			backgroundImage: `linear-gradient(90deg, #${value.substr(6, 6)} 0% 15%, #${value.substr(12, 6)} 17% 32%, #${value.substr(18, 6)} 34% 49%, #${value.substr(24, 6)} 51% 66%, #${value.substr(30, 6)} 68% 83%, #${value.substr(36, 6)} 85% 100%)`
 		} : {}
 		return (
-			<span style={colorStyle} className={`${className} eth-address`} >
-				<span className={valid ? "address-bg" : "address-err"}>
+			<span style={colorStyle} className={`${className}`} >
+				<span className={valid ? "bg" : "err"}>
 					{blocky && 
 						<span className="blocky">
-							<Blockies seed={address.toLowerCase()} size={blockySize} scale={blockyScale} />
+							<Blockies seed={value.toLowerCase()} size={blockySize} scale={blockyScale} />
 						</span>}
-					<span className={control ? "address-indicator" : "address-text" } >
-						<strong>{address.substr(0, 6)}</strong><small>{address.substr(6, 36)}</small><strong>{address.substr(36, 6)}</strong>
+					<span className={control ? "indicator" : "text" } >
+						<strong>{value.substr(0, 6)}</strong><small>{value.substr(6, 36)}</small><strong>{value.substr(36, 6)}</strong>
 					</span>
 					{ control ? 
 					<span 
-						className="address-control"
+						className="control"
 						ref={this.controlRef} 
 						placeholder="0x0000000000000000000000000000000000000000"
 						onKeyPress={(event) => this.onKeyPress(event)} 
