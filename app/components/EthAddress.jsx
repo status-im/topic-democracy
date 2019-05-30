798d6555a0039d19fa47d40279db99dc4965c0f7
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2019-05-30 15:18:37 -0300

    update ethaddress

--- app/components/EthAddress.jsx
+++ app/components/EthAddress.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Blockies from 'react-blockies';
+import { Overlay, Tooltip } from 'react-bootstrap';
+import copy from 'copy-to-clipboard';
 import './EthAddress.css';
+import ClipIcon from "./Icon/Clip";
 
+const nullAddress = "0x0000000000000000000000000000000000000000"
 class EthAddress extends React.Component {
 	static propTypes = {
 		className: PropTypes.string,
-		value: PropTypes.string,
+		address: PropTypes.string,
 		defaultValue: PropTypes.string,
 		colors: PropTypes.bool,
 		blocky: PropTypes.bool,
@@ -19,9 +23,11 @@ class EthAddress extends React.Component {
 
 	static defaultProps = {
 		className: 'eth-address',
-		defaultValue: "0x0000000000000000000000000000000000000000",
+		value: null,
+		defaultValue: nullAddress,
 		colors: true,
 		control: false,
+		allowZero: true,
 		blocky: true,
 		blockySize: 8,
 		blockyScale: 4,
@@ -32,23 +38,87 @@ class EthAddress extends React.Component {
 	constructor(props) {
 		super(props);
 		this.controlRef = React.createRef();
+		this.attachRef = containerRef => this.setState({ containerRef });
 		this.state = {
-			value: props.value != undefined ? props.value : props.defaultValue,
+			value: null,
+			address: nullAddress,
+			ensReverse: null,
+			valid: false,
+			tooltipVisible: false 
 		};
 	}
 
 
 	componentDidMount() {
-		if(this.props.control)
-        	this.controlRef.current.textContent = this.state.value;
+		this.setValue(this.props.value || this.props.defaultValue);
     }
 
 	componentDidUpdate(prevProps, prevState) {
         if (prevProps.value != this.props.value && this.props.value != this.state.value) {
-            this.setState({value: this.props.value});
+            this.setValue(this.props.value);
+		}
+		if(prevState.value != this.state.value) {
+			this.checkValue();	
+		}
+		if(prevState.address != this.state.address) {
+			this.checkAddress();
 		}
     }
 
+	setValue(value){
+		if(this.props.control) {
+			this.controlRef.current.textContent = value;
+		}
+		this.setState({value, address: nullAddress, ensReverse: null});
+	}
+
+	checkAddress() {
+		const { value, address } = this.state;
+		if(address != nullAddress){
+			EmbarkJS.Names.lookup(address, (err, name) => {
+				if(err){
+					console.log(address, err)
+				}
+				this.setState({ensReverse: name});
+				this.props.onChange(address, value, name);
+			})
+		} else {
+			this.setState({ensReverse: null});
+			this.props.onChange(address, value, null);
+		}
+	}	
+	
+	checkValue() {
+		const { value } = this.state;
+		if(value == null){
+			this.setState({address: nullAddress, valid: this.props.allowZero});
+		}else if(value.startsWith("0x")) {
+			const valid = (this.props.allowZero || value != nullAddress) && /^(0x)?[0-9a-f]{40}$/i.test(value);
+			if (valid) {
+				this.setState({address: value, valid});
+			} else {
+				this.setState({address: nullAddress, valid});
+			}
+		} else {
+			EmbarkJS.Names.resolve(value, (err, result) => {
+				if(err){
+					this.setState({address: nullAddress, valid: false});
+				} else {
+					const valid = !err && result != nullAddress;
+					this.setState({address: result, valid});	
+				}
+			});
+		}
+	}
+	onClick = () => {
+		if (!this.props.control) {
+			copy(this.state.address);
+			this.setState({ tooltipText: "Copied", tooltipVisible: true });
+			clearTimeout(this.timeout);
+			this.timeout = setTimeout(() => { this.setState({ tooltipVisible: false }) }, 1000)
+		}
+	}
+	
     onKeyPress(event) {
         if (event.charCode === 13) {
             event.preventDefault();
@@ -56,41 +126,29 @@ class EthAddress extends React.Component {
     }
 
     onKeyUp(event) {
-		let text = this.controlRef.current.textContent;
-		if(text.length == 0){
-			text = "0x0000000000000000000000000000000000000000";
-		}
-		this.setState({ value: text });
-		this.notifyListeners(text);
-	}
-	
-	notifyListeners(text) {
-		if (/^(0x)?[0-9a-f]{40}$/i.test(text)) {
-			this.props.onChange(text);
-		} else {
-			this.props.onChange(null);
-		}
+		clearTimeout(this.keyWait);
+		this.keyWait = setTimeout(() => {this.setState({ value: this.controlRef.current.textContent })}, 200);
 	}
 
     handlePaste(event) {
-        var clipboardData, pastedData;
+		event.stopPropagation();
+		event.preventDefault();
+		var clipboardData, pastedData;
         clipboardData = event.clipboardData || window.clipboardData;
-        pastedData = clipboardData.getData('Text');
-        if (/^(0x)?[0-9a-f]{40}$/i.test(pastedData)) {
-            event.stopPropagation();
-            event.preventDefault();
-            this.controlRef.current.textContent = pastedData;
-            this.setState({ value: pastedData });
-		} else {
-			this.setState({ value: this.controlRef.current.textContent });
-		}
-		this.notifyListeners(this.controlRef.current.textContent);
+		pastedData = clipboardData.getData('Text');
+		this.controlRef.current.textContent = pastedData;
+		this.setState({ value: pastedData });
     }
 
     focus() {
         this.controlRef.current.focus();
 	}
 	
+
+	getBackgroundGradient(address) {
+		return `linear-gradient(90deg, #${address.substr(2, 6)} 0% 14%, #${address.substr(8, 6)} 14% 28%, #${address.substr(14, 6)} 28% 42%, #${address.substr(19, 6)} 43% 57%, #${address.substr(24, 6)} 58% 72%, #${address.substr(30, 6)} 72% 86%, #${address.substr(36, 6)} 86% 100%)`
+	}
+
 	render() {
 		const {
 			disabled,
@@ -101,39 +159,56 @@ class EthAddress extends React.Component {
 			blockyScale,
 			control
 		} = this.props;
-		const value = this.state.value ? this.state.value : "0x0000000000000000000000000000000000000000";
-		let valid = /^(0x)?[0-9a-f]{40}$/i.test(value);
-		const colorStyle = colors && valid ? {
-			backgroundImage: `linear-gradient(90deg, #${value.substr(6, 6)} 0% 15%, #${value.substr(12, 6)} 17% 32%, #${value.substr(18, 6)} 34% 49%, #${value.substr(24, 6)} 51% 66%, #${value.substr(30, 6)} 68% 83%, #${value.substr(36, 6)} 85% 100%)`
+		const { ensReverse, value, valid, address } = this.state;
+		const { containerRef, tooltipVisible, tooltipText } = this.state;
+		const colorStyle = colors ? {
+			backgroundImage: this.getBackgroundGradient(address)
 		} : {}
 		return (
-			<span style={colorStyle} className={`${className}`} >
+			<span ref={this.attachRef} style={colorStyle} onClick={this.onClick} className={`${className} ${valid ? '': 'err' }`} >
 				<span className={valid ? "bg" : "err"}>
-					{blocky && 
-						<span className="blocky">
-							<Blockies seed={value.toLowerCase()} size={blockySize} scale={blockyScale} />
-						</span>}
+					{blocky && valid &&	 
+						<Blockies className="blocky" seed={address.toLowerCase()} size={blockySize} scale={blockyScale} />
+					}
 					<span className={control ? "indicator" : "text" } >
-						<strong>{value.substr(0, 6)}</strong><small>{value.substr(6, 36)}</small><strong>{value.substr(36, 6)}</strong>
+						{ensReverse && 
+						<span className="ens-reverse">
+							<small>{ensReverse}</small>
+						</span>}
+						<span> 
+							<strong>{address.substr(0, 6)}</strong><small>{address.substr(6, 36)}</small><strong>{address.substr(36, 6)}</strong>
+						</span>
 					</span>
 					{ control ? 
 					<span 
 						className="control"
 						ref={this.controlRef} 
-						placeholder="0x0000000000000000000000000000000000000000"
+						placeholder={nullAddress}
 						onKeyPress={(event) => this.onKeyPress(event)} 
 						onKeyUp={(event) => this.onKeyUp(event)}
 						onPaste={(event) => this.handlePaste(event)}
 						contentEditable={!disabled} 
 						/>
 					: 
-					<span /> 
+					<span className="clip-icon">
+						<ClipIcon width={15} fill="#CCC"/>
+					</span> 
 					}
+					<Overlay target={containerRef} show={tooltipVisible} placement="bottom">
+						{(props) => {
+							delete props['show'];
+							return (
+								<Tooltip id="address-tooltip" {...props} >{tooltipText}</Tooltip>
+							)
+						}}
+					</Overlay>
 				</span>
 			</span>		
 		)
 	}
-	
-}
+
+	componentWillUnmount() {
+		clearTimeout(this.timeout);
+	}}
 
 export default EthAddress;
