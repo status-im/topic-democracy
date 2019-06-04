ebdb502cce48bd38eb6e7405f2df4983700ea7ee
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2019-06-04 03:07:37 -0300

    add icons

---
+++ app/components/icon/Execute.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default ({
+  style = {},
+  fill = "#000",
+  width = "1em",
+  className = "",
+  viewBox = "0 0 20 20"
+}) => (
+  <svg
+    width={width}
+    style={style}
+    height={width}
+    viewBox={viewBox}
+    xmlns="http://www.w3.org/2000/svg"
+    className={`svg-icon ${className || ""}`}
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+  >
+    <path 
+      fill={fill}
+      d="M17.218,2.268L2.477,8.388C2.13,8.535,2.164,9.05,2.542,9.134L9.33,10.67l1.535,6.787c0.083,0.377,0.602,0.415,0.745,0.065l6.123-14.74C17.866,2.46,17.539,2.134,17.218,2.268 M3.92,8.641l11.772-4.89L9.535,9.909L3.92,8.641z M11.358,16.078l-1.268-5.613l6.157-6.157L11.358,16.078z" 
+    />
+  </svg>
+);
